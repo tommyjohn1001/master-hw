0c5f92b826fc70ef3852f2361d406a1f172735c5
Hoang Le <tommyle133@gmail.com>
2024-06-18 23:28:27 +0300

    FInish most part of pipeline

--- README.md
+++ README.md
@@ -1,5 +1,12 @@
-## Homework for Master Degree in Helsinki
+Thesis
 
-1. Notes with libraries
+# 1. Notes with libraries
 
 Use file `py.yaml` to set up environment.
+
+# 2. Dataset `TimeCutoffDataset`
+
+## 2.1. Functional requirements
+
+- Must be reproducible: `reproducibility` and `seed` in config
+-
--- pipeline/utils.py
+++ pipeline/utils.py
@@ -1,6 +1,35 @@
+import logging
 from datetime import datetime
+from pathlib import Path
+
+from loguru import logger
+
+tag = datetime.now().strftime("%b%d_%H%M%S")
+PATH_ROOT = Path("logs") / f"{tag}"
+
+PATH_ROOT.mkdir(parents=True, exist_ok=True)
 
 
-def get_cur_dt() -> str:
-    # TODO: HoangLe [Jun-17]: Complete this
-    return datetime.now().strftime("%d")
+def get_path_log():
+    return (PATH_ROOT / "log").as_posix()
+
+
+def get_path_conf():
+    return (PATH_ROOT / "conf.yml").as_posix()
+
+
+def get_path_dir_ckpt():
+    return (PATH_ROOT / "ckpts").as_posix()
+
+
+def get_path_tune_log():
+    return (PATH_ROOT / "tune_result.json").as_posix()
+
+
+def get_logger():
+    fh = logging.FileHandler(get_path_log())
+
+    logger.add(fh)
+    logger.add(get_path_log())
+
+    return logger
