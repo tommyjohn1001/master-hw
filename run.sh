5fc7684e8b513af64e9774bb811a3c58d6ab9895
Hoang Le <tommyle133@gmail.com>
2024-07-09 21:48:25 +0300

    Small fix in config files

--- run.sh
+++ run.sh
@@ -8,7 +8,9 @@ date
 echo
 echo
 
-python run_pipeline.py -m Caser -d ml-1m -l BPR -t 976324045 --use_cutoff --reproducible
+python run_pipeline.py -m NPE -d ml-1m -l CE -t 976324045 --use_cutoff --reproducible
+# ml-100k 884471835
+# ml-1m 976324045
 
 echo
 echo
